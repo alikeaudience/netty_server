a974a306a35c28308111781bdcab14a992dd5407
Jiwei <jiweiliphd@gmail.com>
2016-10-17 17:12:42 +0800

    Add support CORS

--- src/jsonserver/alikeaudience/com/HttpJsonServerInitializer.java
+++ src/jsonserver/alikeaudience/com/HttpJsonServerInitializer.java
@@ -7,6 +7,9 @@ import io.netty.handler.codec.http.HttpContentDecompressor;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpRequestDecoder;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.cors.CorsConfig;
+import io.netty.handler.codec.http.cors.CorsConfigBuilder;
+import io.netty.handler.codec.http.cors.CorsHandler;
 import io.netty.handler.ssl.SslContext;
 
 /**
@@ -21,6 +24,7 @@ public class HttpJsonServerInitializer extends ChannelInitializer<SocketChannel>
 
     @Override
     public void initChannel(SocketChannel ch) {
+        CorsConfig corsConfig = CorsConfigBuilder.forAnyOrigin().allowNullOrigin().allowCredentials().allowedRequestHeaders("X-Requested-With, Content-Type, Content-Length").build();
         ChannelPipeline p = ch.pipeline();
         if (sslCtx != null) {
             p.addLast(sslCtx.newHandler(ch.alloc()));
@@ -29,7 +33,7 @@ public class HttpJsonServerInitializer extends ChannelInitializer<SocketChannel>
         p.addLast("decoder", new HttpRequestDecoder());
         p.addLast("decompress", new HttpContentDecompressor());
         p.addLast("aggregator", new HttpObjectAggregator(1048576));
-
+        p.addLast(new CorsHandler(corsConfig));
         p.addLast(new HttpJsonServerHandler());
     }
 }
